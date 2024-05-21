c6ff155c96406881338fd6ebd455fff430c222e3
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-21 13:10:45 -0700

    Time: 364 ms (60.12%), Space: 30.8 MB (29.85%) - LeetHub

--- 2474-minimum-adjacent-swaps-to-make-a-valid-array/2474-minimum-adjacent-swaps-to-make-a-valid-array.py
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/2474-minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -1,41 +1,26 @@
 class Solution:
     def minimumSwaps(self, nums: List[int]) -> int:
 
-        iterator = 0
         noOfSwaps = 0
-        n = len(nums)-1
-        maxValue = float("-inf")
-        maxIndex = -1
-        minValue = float("inf")
-        minIndex = -1
+        n = len(nums)
 
-        for index, num in enumerate(nums):
-            if num < minValue:
+        if n == 1:
+            return 0
+
+        # maxValue = float("-inf")
+        maxIndex = 0
+        # minValue = float("inf")
+        minIndex = 0
+
+        for index in range(1, n):
+            if nums[index] < nums[minIndex]:
                 minIndex = index
-                minValue = num
-            if num >= maxValue:
+                # minValue = nums[index]
+            if nums[index] >= nums[maxIndex]:
                 maxIndex = index
-                maxValue = num
+                # maxValue = nums[index]
 
-        noOfSwaps = (minIndex - 0) + (n - maxIndex)
+        noOfSwaps = (minIndex) + (n - 1 - maxIndex)
         if minIndex > maxIndex:
             noOfSwaps -= 1
         return noOfSwaps
-        # iterator = minIndex
-        # # calculate the swaps needed for highest number
-        # while iterator < n - 1:
-        #     if nums[iterator] > nums[iterator + 1]:
-        #         nums[iterator + 1], nums[iterator] = nums[iterator], nums[iterator + 1]
-        #         noOfSwaps += 1
-        #     iterator += 1
-
-        # # calculate the swaps needed for lowest number
-        # iterator = n - 1
-
-        # while iterator > 0:
-        #     if nums[iterator] < nums[iterator - 1]:
-        #         nums[iterator - 1], nums[iterator] = nums[iterator], nums[iterator - 1]
-        #         noOfSwaps += 1
-        #     iterator -= 1
-
-        # return noOfSwaps
