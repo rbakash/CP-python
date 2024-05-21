39eb41f4df9af9b56a6fd24875c7c4cdf794ff01
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-20 18:02:11 -0700

    Time: 131 ms (5.25%), Space: 17.2 MB (9.04%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -3,14 +3,15 @@ class Solution:
         operators = set(["+", "-", "*", "/"])
         print(operators)
         stack = []
+       
         for token in tokens:
             if token not in operators:
                 stack.append(token)
             else:
                 rightOperand = stack.pop()
                 leftOperand = stack.pop()
-                print(str(leftOperand) +token + str(rightOperand))
-                value = eval("".join(str(leftOperand) +token + str(rightOperand)))
+                print(str(leftOperand) + token + str(rightOperand))
+                value = eval("".join(str(leftOperand) + token + str(rightOperand)))
                 stack.append(int(value))
 
         return int(stack[-1])
