8b2d743ccda403b34fadc2941a3a266cb1d20f86
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-23 20:11:40 -0700

    Time: 72 ms (89.58%), Space: 17.4 MB (99.78%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        stringSLength = len(s)
+        stringPLength = len(p)
+        if stringSLength < stringPLength:
+            return []
+        print(stringPLength, stringSLength)
+        anagramIndex = []
+        stringSFrequency, stringPFrequency = [0] * 26, [0] * 26
+
+        # create frequency map
+        for index in range(stringPLength):
+            stringPFrequency[ord(p[index]) - ord("a")] += 1
+            # stringSFrequency[ord(s[index]) - ord("a")] += 1
+
+        for index in range(stringSLength):
+
+            stringSFrequency[ord(s[index]) - ord("a")] += 1
+            if index >= stringPLength:
+
+                stringSFrequency[ord(s[index - stringPLength]) - ord("a")] -= 1
+
+            if stringSFrequency == stringPFrequency:
+                anagramIndex.append(index - stringPLength + 1)
+
+        return anagramIndex
