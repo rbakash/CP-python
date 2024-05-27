566c20e398fe2350d46d1eeb8d06403479388feb
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-27 10:50:52 -0700

    Time: 33 ms (67.3%), Space: 16.4 MB (98.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        bestWays = [0] * (n + 1)
-        bestWays[1] = 1
-        bestWays[2] = 2
+        if n <= 2:
+            return n
+        first, second = 1, 2
 
         for index in range(3, n + 1):
-            bestWays[index] = bestWays[index - 1] + bestWays[index - 2]
-        return bestWays[n]
+            ways = first + second
+            first = second
+            second = ways
+        return second
