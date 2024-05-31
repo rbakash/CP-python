e776ac72925f7707f39371f266b6a6b3fdc84d25
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-31 10:20:50 -0700

    Time: 309 ms (11.27%), Space: 24.4 MB (19.57%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -5,6 +5,7 @@ class Solution:
             return noOfIslands
         rows = len(grid)
         cols = len(grid[0])
+        visited = set()
 
         def dfs(currentRow, currentCol):
             if (
@@ -22,9 +23,29 @@ class Solution:
             dfs(currentRow, currentCol - 1)
             dfs(currentRow, currentCol + 1)
 
+        def bfs(row, col):
+            queue = deque()
+            queue.append((row, col))
+            visited.add((row, col))
+
+            while queue:
+                currentRow, currentCol = queue.popleft()
+                directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+                for dr, dc in directions:
+                    r, c = currentRow + dr, currentCol + dc
+                    if (
+                        r in range(rows)
+                        and c in range(cols)
+                        and grid[r][c] == "1"
+                        and (r, c) not in visited
+                    ):
+                        visited.add((r, c))
+                        queue.append((r, c))
+
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == "1":
-                    dfs(row, col)
+                if grid[row][col] == "1" and (row, col) not in visited:
+                    # dfs(row, col)
+                    bfs(row, col)
                     noOfIslands += 1
         return noOfIslands
