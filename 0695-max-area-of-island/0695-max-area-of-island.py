a949405d3b532e293e5a047ed8456b02750b0356
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-05-31 15:15:56 -0700

    Time: 118 ms (31.31%), Space: 17.1 MB (59.19%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        
+        if not grid:
+            return 0
         rows = len(grid)
         cols = len(grid[0])
         maxArea = 0
         visited = set()
-        if not grid:
-            return 0
 
         direction = [[0, -1], [0, 1], [-1, 0], [1, 0]]
 
         def bfs(currentRow, currentCol):
 
             queue = deque()
-            
+
             queue.append((currentRow, currentCol))
             visited.add((currentRow, currentCol))
             currentArea = 1
@@ -34,7 +33,7 @@ class Solution:
 
         for row in range(rows):
             for col in range(cols):
-                if grid[row][col] == 1  and (row,col) not in visited:
+                if grid[row][col] == 1 and (row, col) not in visited:
 
                     currentArea = bfs(row, col)
                     maxArea = max(currentArea, maxArea)
