9b1bc9fd527ac44a787243bc430cbc52e1461995
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-01 10:56:56 -0700

    Time: 189 ms (56.17%), Space: 22.8 MB (35.68%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -16,11 +16,11 @@ class Solution:
                     visited.add((row, col))
 
         def addRooms(r,c):
-            if (r not in range(rows) or c not in range(cols) or rooms[r][c] == -1 or (r, c) in visited ):
+            if r <0 or r == rows or c < 0 or c == cols or rooms[r][c] == -1 or (r, c) in visited:
                 return
             queue.append((r, c))
             visited.add((r, c))
-            
+
         distance = 0
         while queue:
             for idx in range(len(queue)):
@@ -29,5 +29,6 @@ class Solution:
                 for r, c in direction:
                     r, c = row + r, col + c
                     addRooms(r, c)
-                   
             distance += 1
+
+        return None
