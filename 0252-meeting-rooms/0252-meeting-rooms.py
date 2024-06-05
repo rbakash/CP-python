608fd13abfba7ea3fc74100ff504bcbc20c0e182
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-05 14:26:18 -0700

    Time: 73 ms (51.93%), Space: 19.7 MB (97.74%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,10 +1,8 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         intervals.sort()
-        print(intervals)
 
         for index in range(1, len(intervals)):
-            print(intervals[index-1][1],intervals[index][0])
             if intervals[index-1][1]-intervals[index][0]>0:
                 return False
         return True
