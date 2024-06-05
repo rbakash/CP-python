70b8815e4d0284b7c5c8c26b53b754028b2e45b8
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-05 09:07:54 -0700

    Time: 1567 ms (83.06%), Space: 60.8 MB (92.4%) - LeetHub

---
+++ 2026-merge-triplets-to-form-target-triplet/2026-merge-triplets-to-form-target-triplet.py
@@ -0,0 +1,26 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        allIndexes = set()
+        a,b,c,=0,0,0
+        for eachTriplet in triplets:
+
+            # if any of the value is greater than triplet ignore that triplet
+            if (
+                eachTriplet[0] > target[0]
+                or eachTriplet[1] > target[1]
+                or eachTriplet[2] > target[2]
+            ):
+                continue
+
+            a = max(a,eachTriplet[0])
+            b=max(b,eachTriplet[1])
+            c=max(c,eachTriplet[2])
+
+            if (
+               a== target[0]
+                and b == target[1]
+                and c == target[2]
+            ):
+                return True
+
+        return False
