896203956097f2b745082a3d203dfe75fa8ce41b
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-09 09:25:21 -0700

    Time: 4295 ms (100%), Space: 16.6 MB (100%) - LeetHub

--- 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.py
+++ 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.py
@@ -1,12 +1,6 @@
 class Solution:
     def valueAfterKSeconds(self, n: int, k: int) -> int:
-        # prevSum =1
-        # currentSum =0
-        # seriesIterator =2
-        # for num in range(k+1):
-        #     currentSum = prevSum + (seriesIterator*(seriesIterator+1)/2)
-        #     prevSum = currentSum
-        #     seriesIterator+=1
+        
         a = [1] * n
         largeMod = 10**9 + 7
         for eachK in range(k):
