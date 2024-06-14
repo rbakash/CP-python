7b244130fe9a4af2e0df5c2cfe1e5f8d4d5a2835
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-14 14:38:51 -0700

    Time: 30 ms (93.04%), Space: 16.5 MB (21.03%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,15 +4,16 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zeroIndex = 0
-        n = len(nums)
-        secondIndex = n-1
-        iterator =0
+        secondIndex = len(nums) - 1
+        iterator = 0
+
         while iterator <= secondIndex:
             if nums[iterator] == 0:
                 nums[zeroIndex], nums[iterator] = nums[iterator], nums[zeroIndex]
                 zeroIndex += 1
-                iterator+=1
+                iterator += 1
             elif nums[iterator] == 2:
                 nums[secondIndex], nums[iterator] = nums[iterator], nums[secondIndex]
                 secondIndex -= 1
-            else : iterator+=1
+            else:
+                iterator += 1
