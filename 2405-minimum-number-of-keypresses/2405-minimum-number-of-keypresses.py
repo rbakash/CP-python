2ad17c1c9841dd79425aa3b298dabf32c7212cb3
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-19 14:47:26 -0700

    Time: 72 ms (15.11%), Space: 17.6 MB (42.6%) - LeetHub

--- 2405-minimum-number-of-keypresses/2405-minimum-number-of-keypresses.py
+++ 2405-minimum-number-of-keypresses/2405-minimum-number-of-keypresses.py
@@ -1,6 +1,7 @@
 class Solution:
     def minimumKeypresses(self, s: str) -> int:
         n = len(s)
+        keypad=defaultdict(list)
         # You can distribute each key per number
         if n <= 9:
             return n
@@ -10,11 +11,12 @@ class Solution:
 
         for eachCharacter in s:
             frequency[eachCharacter] += 1
-        sortedList = sorted(frequency.values(), reverse=True)
+        sortedList = sorted(frequency.items(),key= lambda item:item[1], reverse=True)
 
-        for idx, value in enumerate(sortedList):
+        for idx, (char,occurence) in enumerate(sortedList):
 
             iterator = iterator + 1 if idx % 9 == 0 else iterator
-            keyPresses += value * iterator
-
+            keyPresses += occurence * iterator
+            keypad[(idx+1)%9].append(char)
+        print(keypad)
         return keyPresses
