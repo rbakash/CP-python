8b8d763c97c797ac87fe475a9e7dff9e8acd4db6
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-19 10:33:36 -0700

    Time: 52 ms (58.1%), Space: 16.4 MB (70.62%) - LeetHub

--- 3367-find-the-sum-of-encrypted-integers/3367-find-the-sum-of-encrypted-integers.py
+++ 3367-find-the-sum-of-encrypted-integers/3367-find-the-sum-of-encrypted-integers.py
@@ -3,12 +3,10 @@ class Solution:
         result = 0
         for num in nums:
             maxDigit = 0
-            digitCount = 0
             count = 0
             while num > 0:
                 maxDigit = max(maxDigit, num % 10)
                 num = num // 10
-                digitCount += 1
                 count = count * 10 + 1
             result += maxDigit * count
 
