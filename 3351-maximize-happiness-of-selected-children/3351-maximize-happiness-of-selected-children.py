59b0396ea2e65f2c7401d39f9c0202d2a5b0d0f7
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-20 21:22:14 -0700

    Time: 806 ms (61.32%), Space: 43.4 MB (63.68%) - LeetHub

--- 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/3351-maximize-happiness-of-selected-children.py
@@ -1,12 +1,13 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        maxheap = [-num for num in happiness]
-        heapify(maxheap)
+        # maxheap = [-num for num in happiness]
+        # heapify(maxheap)
+        happiness.sort(reverse=True)
         timeElapsed =0
         maxHappiness = 0
 
         while k > 0:
-            maxElement = heappop(maxheap)*-1
+            maxElement = happiness[timeElapsed]
             if maxElement - timeElapsed > 0:
                 maxHappiness += (maxElement-timeElapsed)
             k-=1
