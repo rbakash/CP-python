dfb5f0d479f48f151f8d42398f6dfb3a13f49c01
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-22 11:38:56 -0700

    Time: 35 ms (54.98%), Space: 16.5 MB (77.22%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,24 +1,18 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        # orderChar=set([charac for charac in order])
         sChar=set([charac for charac in s])
         frequency  = defaultdict(int)
         for each in s:
             frequency[each]+=1
-
-        result=[]
-        print(frequency)
-        print(sChar)
+        result=""
 
         for eachCharacter in order:
             if eachCharacter in frequency:
-                for count in range(frequency[eachCharacter]):
-                    result.append(eachCharacter)
+                result+=(eachCharacter * frequency[eachCharacter])
                 del frequency[eachCharacter]
         
         for char, count in frequency.items():
-            for count in range(count):
-                result.append(char)
+            result+=(char * count)
         
-        return "".join(result)
+        return result
         
