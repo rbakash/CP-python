9fb03ae5dfdec4c4cd18bcd717ac0c1922ce1c82
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-22 10:59:58 -0700

    Time: 1066 ms (100%), Space: 20.2 MB (100%) - LeetHub

--- 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
+++ 3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3477-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.py
@@ -3,9 +3,13 @@ class Solution:
         minOperations = 0
         for num in nums:
             if num ==0:
+                # If you have flipped even times, then this is final state of the num
+                # Need to flip this
                 if minOperations %2 ==0:
                     minOperations+=1
             else:
+                # The 1 might be flipped before, so we need to check the flipped state
+                # the 1 will be 0 if we have flipped odd times before
                 if minOperations % 2 ==1:
                     minOperations +=1
         return minOperations
