1d0d0ad0fcd5d5751611c77af8350a003250cce0
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-23 09:44:40 -0700

    Time: 396 ms (99.15%), Space: 30.4 MB (91.88%) - LeetHub

--- 2354-minimum-health-to-beat-game/2354-minimum-health-to-beat-game.py
+++ 2354-minimum-health-to-beat-game/2354-minimum-health-to-beat-game.py
@@ -1,4 +1,4 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
-        return (sum(damage) - (armor if max(damage) > armor else max(damage))) +1
+        return sum(damage) - min(max(damage),armor)+1
         
