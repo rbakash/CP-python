4977be9dfc08300fd4853306c47826075d558019
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-28 15:57:39 -0700

    Time: 1048 ms (37.17%), Space: 31.1 MB (30.86%) - LeetHub

---
+++ 1966-frequency-of-the-most-frequent-element/1966-frequency-of-the-most-frequent-element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        start, maxFrequency, currentSum = 0, 0, 0
+        nums.sort()
+        for end, num in enumerate(nums):
+            currentSum += nums[end]
+            noOfOperations = nums[end] * (end - start + 1) - currentSum
+            while noOfOperations > k:
+                currentSum -= nums[start]
+                start += 1
+                noOfOperations = nums[end] * (end - start + 1) - currentSum
+            maxFrequency = max(maxFrequency, (end - start + 1))
+
+        return maxFrequency
