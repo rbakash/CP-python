cd0c25f959d1cfc32a51d9a05735f2c124251bbe
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-29 13:00:19 -0700

    Time: 1271 ms (77.65%), Space: 20.6 MB (49.43%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
@@ -1,14 +1,26 @@
 class SparseVector:
     def __init__(self, nums: List[int]):
-        self.vector=nums
+        self.vector = []
+        for index, num in enumerate(nums):
+            if num != 0:
+                self.vector.append((index, num))
 
     # Return the dotProduct of two sparse vectors
-    def dotProduct(self, vec: 'SparseVector') -> int:
-        result =0
-        for index in range(len(self.vector)):
-            result += self.vector[index]* vec.vector[index]
+    def dotProduct(self, vec: "SparseVector") -> int:
+        result = 0
+        iteratorForV1, iteratorForV2 = 0, 0
+        while iteratorForV1 < len(self.vector) and iteratorForV2 < len(vec.vector):
+            if self.vector[iteratorForV1][0] < vec.vector[iteratorForV2][0]:
+                iteratorForV1 += 1
+            elif self.vector[iteratorForV1][0] > vec.vector[iteratorForV2][0]:
+                iteratorForV2 += 1
+            else:
+                result += self.vector[iteratorForV1][1] * vec.vector[iteratorForV2][1]
+                iteratorForV1 += 1
+                iteratorForV2 += 1
+
         return result
-        
+
 
 # Your SparseVector object will be instantiated and called as such:
 # v1 = SparseVector(nums1)
