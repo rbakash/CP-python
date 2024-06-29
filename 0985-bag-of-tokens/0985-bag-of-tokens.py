bc2d09f340dd29f1bf263ce5d4b6f22070e9a617
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-29 14:26:36 -0700

    Time: 44 ms (92.8%), Space: 16.5 MB (95.14%) - LeetHub

---
+++ 0985-bag-of-tokens/0985-bag-of-tokens.py
@@ -0,0 +1,16 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        tokens.sort()
+        low, high,score=0,len(tokens)-1,0
+        while low<= high:
+            if tokens[low] <= power:
+                score+=1
+                power-=tokens[low]
+                low+=1
+            elif low< high and score > 0:
+                power +=tokens[high]
+                high-=1
+                score-=1
+            else:
+                return score
+        return score
