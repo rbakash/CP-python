5f8daca72ba4d3fd6fb26b3fe5f84efa8eae1729
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-29 11:56:18 -0700

    Time: 34 ms (68.44%), Space: 16.4 MB (81.83%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,29 +1,23 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        wordIterator,abbrIterator, length =0,0,0
+        wordIterator, abbrIterator, length = 0, 0, 0
         n = len(word)
         while abbrIterator < len(abbr) and wordIterator < len(word):
             if abbr[abbrIterator].isdigit():
-                if abbr[abbrIterator] =='0':
+                if abbr[abbrIterator] == "0":
                     return False
                 # Traverse till a char is found to get the length
-                index = abbrIterator
+                index = 0
                 print(abbrIterator)
-                while index < len(abbr) and abbr[index].isdigit():
-                    index+=1
-                length = int(abbr[abbrIterator:index])
-                print(length)
-                wordIterator += length
-                abbrIterator =index
-                print(abbrIterator)
-                # print(word[wordIterator],abbr[abbrIterator])
+                while abbrIterator < len(abbr) and abbr[abbrIterator].isdigit():
+                    index = 10 * index + int(abbr[abbrIterator])
+                    abbrIterator+=1
+                wordIterator += index
+
             else:
                 if word[wordIterator] != abbr[abbrIterator]:
                     return False
-                wordIterator+=1
-                abbrIterator+=1
-        
-        return wordIterator == len(word) and  abbrIterator == len(abbr)
+                wordIterator += 1
+                abbrIterator += 1
 
-                 
-                
+        return wordIterator == len(word) and abbrIterator == len(abbr)
