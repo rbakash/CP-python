2ff74b67297fe7a056ef6cca579f3b904264254f
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-29 18:29:38 -0700

    Time: 342 ms (83.58%), Space: 23.5 MB (24.79%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.py
+++ 0917-boats-to-save-people/0917-boats-to-save-people.py
@@ -1,16 +1,17 @@
 class Solution:
     def numRescueBoats(self, peoples: List[int], limit: int) -> int:
-        end, minimumBoat,start = len(peoples)-1, 0,0
+        end, minimumBoat, start = len(peoples) - 1, 0, 0
         peoples.sort()
+        # If there are people with weight == limit, they can combined with others. Skip them
         while peoples[end] == limit:
             minimumBoat += 1
             end -= 1
         # When start equals end, it means only one person is left to be considered.
         # This person still needs a boat, so the loop should process this case to ensure they are not skipped
-        while start<= end:
-            if peoples[start]+peoples[end] <=limit:
+        while start <= end:
+            if peoples[start] + peoples[end] <= limit:
                 # 2 people can be accomodated in the boat
-                start+=1
-            end-=1
-            minimumBoat+=1
+                start += 1
+            end -= 1
+            minimumBoat += 1
         return minimumBoat
