cbb8ed8cb1714555f53151a29abae90ded0b7b39
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-06-29 19:25:01 -0700

    Time: 91 ms (73.18%), Space: 16.5 MB (62.41%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,15 +1,10 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # get all the numbers less than c**0.5
-        numbers =[]
-        for num in range(int(c**0.5)+1):
-            numbers.append(num)
-        print(numbers)
 
         # See if you can make c from numbers
-        start,end = 0,len(numbers)-1
+        start,end = 0,ceil(sqrt(c))
         while start <= end:
-            square = numbers[start]**2 + numbers[end]**2
+            square = start**2 + end**2
             if square == c:
                 return True
             elif square < c:
