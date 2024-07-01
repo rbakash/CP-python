e80debb3c0fd81cd3d72aa64f4c79c733a5f8b8e
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-01 15:56:07 -0700

    Time: 72 ms (5.78%), Space: 17 MB (18.37%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -2,18 +2,23 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         if len(chars) == 1:
             return 1
-        start, currentCount,res = 0,  1, 0
-        while start < len(chars):
+        start, currentCount, res,end = 0, 1, 0, 0
+        while end < len(chars) and start< len(chars):
             print(res, start)
-            while start + currentCount < len(chars) and chars[start + currentCount] == chars[start]:
+            if ( end + currentCount < len(chars) and
+                chars[end + currentCount] == chars[end]
+            ):
                 currentCount += 1
                 # end += 1
-            chars[res] = chars[start]
-            res += 1
-            if currentCount > 1:
-                str_repr = str(currentCount)
-                chars[res:res+len(str_repr)] = list(str_repr)
-                res += len(str_repr)
-            start += currentCount
-            currentCount=1
-        return res
+            else:
+                chars[start]=chars[end]
+                start+=1
+                if currentCount > 1:
+                    str_repr = str(currentCount)
+                    chars[start : start + len(str_repr)] = list(str_repr)
+                    start += len(str_repr)
+                end += currentCount
+                currentCount = 1
+        
+        chars=chars[:start]
+        return len(chars)
