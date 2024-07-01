f2f7c279cc122a69004bff9be6182fccc0607c96
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-01 15:56:38 -0700

    Time: 57 ms (64.02%), Space: 16.8 MB (18.37%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -5,20 +5,19 @@ class Solution:
         start, currentCount, res,end = 0, 1, 0, 0
         while end < len(chars) and start< len(chars):
             print(res, start)
-            if ( end + currentCount < len(chars) and
+            while ( end + currentCount < len(chars) and
                 chars[end + currentCount] == chars[end]
             ):
                 currentCount += 1
                 # end += 1
-            else:
-                chars[start]=chars[end]
-                start+=1
-                if currentCount > 1:
-                    str_repr = str(currentCount)
-                    chars[start : start + len(str_repr)] = list(str_repr)
-                    start += len(str_repr)
-                end += currentCount
-                currentCount = 1
+            chars[start]=chars[end]
+            start+=1
+            if currentCount > 1:
+                str_repr = str(currentCount)
+                chars[start : start + len(str_repr)] = list(str_repr)
+                start += len(str_repr)
+            end += currentCount
+            currentCount = 1
         
         chars=chars[:start]
         return len(chars)
