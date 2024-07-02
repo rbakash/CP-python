dee562749a1a6cbc6378eb7eadaf67075747da60
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-01 17:09:32 -0700

    Time: 33 ms (70.44%), Space: 16.4 MB (99.1%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,17 +1,17 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         # normal approach
-        # version1 = [int(version) for version in version1.split(".")]
-        # version2 = [int(version) for version in version2.split(".")]
+        version1 = [int(version) for version in version1.split(".")]
+        version2 = [int(version) for version in version2.split(".")]
 
-        # for index in range(max(len(version1), len(version2))):
-        #     v1Num = version1[index] if index < len(version1) else 0
-        #     v2Num = version2[index] if index < len(version2) else 0
+        for index in range(max(len(version1), len(version2))):
+            v1Num = version1[index] if index < len(version1) else 0
+            v2Num = version2[index] if index < len(version2) else 0
 
-        #     if v1Num != v2Num:
-        #         return 1 if v1Num > v2Num else -1
+            if v1Num != v2Num:
+                return 1 if v1Num > v2Num else -1
 
-        # return 0
+        return 0
 
         # 2 pointer 
         v1Iterator, v2Iterator = 0, 0
