d96188c9f6b1f5c1cd217ec847b22e45a208b5f5
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-01 19:43:13 -0700

    Time: 596 ms (94.46%), Space: 22.1 MB (77.66%) - LeetHub

---
+++ 0525-contiguous-array/0525-contiguous-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        frequency=defaultdict(int)
+        frequency[0]=-1
+        count,maxLength =0,0
+
+        for index in range(len(nums)):
+            if nums[index] ==0:
+                count-=1
+            else:
+                count+=1
+            if count in frequency:
+                maxLength = max(maxLength, (index -frequency[count]))
+            else:
+                frequency[count]=index
+        return maxLength
+
