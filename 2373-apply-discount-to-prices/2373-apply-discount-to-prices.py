ae5865fda8657bae858c389e2d39be02abc7da2f
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-03 20:01:38 -0700

    Time: 92 ms (90.4%), Space: 18.5 MB (85.05%) - LeetHub

--- 2373-apply-discount-to-prices/2373-apply-discount-to-prices.py
+++ 2373-apply-discount-to-prices/2373-apply-discount-to-prices.py
@@ -1,12 +1,13 @@
 class Solution:
     def discountPrices(self, sentence: str, discount: int) -> str:
-        discount = discount / 100
+        # discount = discount / 100
         words = sentence.split(" ")
+
         for index, eachWord in enumerate(words):
             if eachWord[0] == "$":
                 eachWord = eachWord[1:]
                 if eachWord.isdigit():
-                    newPrice = int(eachWord) - (int(eachWord) * discount)
+                    newPrice = int(eachWord) * (1- (discount/100))
                     eachWord = "$" + "{:.2f}".format(newPrice)
                     words[index] = eachWord
         return " ".join(words)
