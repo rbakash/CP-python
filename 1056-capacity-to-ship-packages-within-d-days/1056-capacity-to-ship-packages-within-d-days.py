8708a7365d84a1574eead32d6544a7e3259189e9
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-05 15:08:50 -0700

    Time: 408 ms (41.83%), Space: 22.3 MB (43.35%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        
+        totalWeight = sum(weights)
+        daysNeeded, remainingWeight = 1, 0
+
+        def canPackageBeShipped(currentShipCapacity):
+            currentLoad,daysNeeded =0,1
+            for index in range(len(weights)):
+                currentLoad += weights[index]
+                # remainingWeight += weights[index]
+                if currentLoad > currentShipCapacity:
+                    currentLoad = weights[index]
+                    daysNeeded += 1
+            return daysNeeded <= days
+        
+        # perform binary search
+        low,high  = max(weights),totalWeight
+        
+        while low <high:
+            mid = low +(high-low)//2
+            if canPackageBeShipped(mid):
+                high = mid; 
+            else:
+                low = mid +1
+        return low
