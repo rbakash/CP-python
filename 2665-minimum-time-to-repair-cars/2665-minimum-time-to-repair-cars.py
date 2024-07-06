64485e422a5c767979691cbc9d69427a197153f3
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-05 18:54:30 -0700

    Time: 1375 ms (15.97%), Space: 20.5 MB (6.06%) - LeetHub

---
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -0,0 +1,18 @@
+class Solution:
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        def calculateNCars(time: int) -> bool:
+            totalCars = 0
+            for rank in ranks:
+                totalCars += math.floor((time // rank) ** 0.5)
+            print(totalCars,time)
+            return totalCars < cars
+
+        # binary search
+        low, high = 1, max(ranks) * (cars) ** 2
+        while low <= high:
+            mid = low + (high - low) // 2
+            if calculateNCars(mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        return low
