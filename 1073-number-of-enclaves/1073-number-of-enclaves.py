e1c3595959c72ce4c5fbadeeeda94d237316dafa
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-07 11:16:45 -0700

    Time: 535 ms (30.94%), Space: 32.8 MB (42.03%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -3,25 +3,26 @@ class Solution:
         rows = len(grid)
         columns = len(grid[0])
         numberOfEnclaves = 0
-        direction = [(0,-1),(0,1),(-1,0),(1,0)]
+        direction = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
-        def dfs(currentRow,currentColumn):
-            if  grid[currentRow][currentColumn]==0:
+        def dfs(currentRow, currentColumn):
+            if grid[currentRow][currentColumn] == 0:
                 return
-            grid[currentRow][currentColumn]=0
-            for rowOffset,colOffset in direction:
-                newRow,newColumn =  currentRow+rowOffset, currentColumn + colOffset
-                if newRow >=0 and newRow < rows and newColumn >= 0 and newColumn < columns:
-                    dfs(newRow,newColumn)
+            grid[currentRow][currentColumn] = 0
+            for rowOffset, colOffset in direction:
+                newRow, newColumn = currentRow + rowOffset, currentColumn + colOffset
+                if 0 <= newRow < rows and 0 <= newColumn < columns:
+                    dfs(newRow, newColumn)
             return
 
         for row in range(rows):
             for col in range(columns):
-                if (row in [0,rows-1] or col in [0,columns-1]) and grid[row][col]==1:
-                    dfs(row,col)
+                if (row in [0, rows - 1] or col in [0, columns - 1]) and grid[row][
+                    col
+                ] == 1:
+                    dfs(row, col)
         for row in range(rows):
             for col in range(columns):
-                if grid[row][col] ==1:
-                    numberOfEnclaves+=1
+                if grid[row][col] == 1:
+                    numberOfEnclaves += 1
         return numberOfEnclaves
-
