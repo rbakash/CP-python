bd0ec5f50d3783e7c08751590fdcaf570b153092
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-06 19:39:11 -0700

    Time: 61 ms (5.79%), Space: 16.8 MB (8.92%) - LeetHub

---
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.py
@@ -0,0 +1,43 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        n = len(equations)
+        parent = [i for i in range(26)]
+        rank = [0] * 26
+
+        def find(nodeX):
+            if nodeX != parent[nodeX]:
+                parent[nodeX] = find(parent[nodeX])
+            return parent[nodeX]
+
+        def union(node1, node2):
+            parent1 = find(node1)
+            parent2 = find(node2)
+            if parent1 != parent2:
+                if rank[parent1] > rank[parent2]:
+                    parent[parent2] = parent1
+                elif rank[parent2] > rank[parent1]:
+                    parent[parent1] = parent2
+                else:
+                    parent[parent2] = parent1
+                    rank[parent1] += 1
+                    return False
+            return True
+
+        for eachEquation in equations:
+            node1, node2, equation = (
+                ord(eachEquation[:1]) - 97,
+                ord(eachEquation[3:]) - 97,
+                eachEquation[1:3],
+            )
+            if equation == "==":
+                union(node1, node2)
+        for eachEquation in equations:
+            node1, node2, equation = (
+                ord(eachEquation[:1]) - 97,
+                ord(eachEquation[3:]) - 97,
+                eachEquation[1:3],
+            )
+            if equation == "!=":
+                if find(node1) == find(node2):
+                    return False
+        return True
