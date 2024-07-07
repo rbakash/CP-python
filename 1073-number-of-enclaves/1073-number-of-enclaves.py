404aaf64989476ea1ec17e91749ccfe2815d78bf
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-07 11:09:41 -0700

    Time: 512 ms (39.72%), Space: 32.9 MB (42.03%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.py
+++ 1073-number-of-enclaves/1073-number-of-enclaves.py
@@ -2,19 +2,15 @@ class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         columns = len(grid[0])
-        print(rows,columns)
         numberOfEnclaves = 0
-        visited=set()
         direction = [(0,-1),(0,1),(-1,0),(1,0)]
+
         def dfs(currentRow,currentColumn):
-            if (currentRow,currentColumn) in visited or grid[currentRow][currentColumn]==0:
+            if  grid[currentRow][currentColumn]==0:
                 return
-            visited.add((currentRow,currentColumn))
             grid[currentRow][currentColumn]=0
             for rowOffset,colOffset in direction:
                 newRow,newColumn =  currentRow+rowOffset, currentColumn + colOffset
-                # print(newRow,newColumn)
-                # print(currentRow,currentColumn)
                 if newRow >=0 and newRow < rows and newColumn >= 0 and newColumn < columns:
                     dfs(newRow,newColumn)
             return
