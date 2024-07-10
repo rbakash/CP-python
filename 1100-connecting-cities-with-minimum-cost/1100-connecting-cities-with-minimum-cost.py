13ba9bca8202fad648615aa8bd180049ca5a5347
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-10 13:10:22 -0700

    Time: 522 ms (41.89%), Space: 23.9 MB (5.01%) - LeetHub

--- 1100-connecting-cities-with-minimum-cost/1100-connecting-cities-with-minimum-cost.py
+++ 1100-connecting-cities-with-minimum-cost/1100-connecting-cities-with-minimum-cost.py
@@ -29,16 +29,42 @@ class Solution:
             return True
 
         # Sort it based on the weight
-        connections.sort(key=lambda node: node[2])
-        minCost, noOfNodesConnected = 0, 0
+        # connections.sort(key=lambda node: node[2])
+        # minCost, noOfNodesConnected = 0, 0
 
-        for node1, node2, weight in connections:
-            if find(node1) == find(node2):
+        # for node1, node2, weight in connections:
+        #     if find(node1) == find(node2):
+        #         continue
+        #     union(node1, node2)
+        #     minCost += weight
+        #     noOfNodesConnected += 1
+        #     if numberOfComponents == 1:
+        #         return minCost
+
+        # return -1
+        """Prim's Algorithm -> pick any random node and keep adding the lowest weight from the neighbor if its not added already"""
+        adj=defaultdict(list)
+        visited = set()
+        minCost=0
+        minHeap=[]
+        minHeap.append([0,1]) # [cost, node]
+
+        # build adj list
+        for node1,node2, weight in connections:
+            adj[node1].append([weight,node2])
+            adj[node2].append([weight,node1])
+            
+        while minHeap and len(visited)< n:
+            currentCost, currentNode = heappop(minHeap)
+            if currentNode in visited:
                 continue
-            union(node1, node2)
-            minCost += weight
-            noOfNodesConnected += 1
-            if numberOfComponents == 1:
-                return minCost
-
-        return -1
+            visited.add(currentNode)
+            minCost +=currentCost
+            for neighborCost, neighborNode in adj[currentNode]:
+                if neighborNode not in visited:
+                    heappush(minHeap,[neighborCost,neighborNode])
+        
+        if len(visited) < n:
+            return -1
+        else:
+            return minCost
