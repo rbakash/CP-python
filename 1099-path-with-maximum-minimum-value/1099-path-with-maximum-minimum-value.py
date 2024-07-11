6cd565331a6efcf01a74b9ea64aeaf8d619b0388
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-10 17:25:41 -0700

    Time: 2754 ms (10.35%), Space: 19 MB (57.33%) - LeetHub

---
+++ 1099-path-with-maximum-minimum-value/1099-path-with-maximum-minimum-value.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maximumMinimumPath(self, grid: List[List[int]]) -> int:
+        dirs = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+
+        def bfs(currentScore: int) -> bool:
+            queue = deque()
+            visited = set()
+            # visited.add((0,0))
+            queue.append((0, 0))  # (row, col)
+            rows = len(grid)
+            cols = len(grid[0])
+
+            while queue:
+                currentRow, currentCol = queue.popleft()
+                if currentRow == rows - 1 and currentCol == cols - 1:
+                    return True
+                for rowOffset, colOffSet in dirs:
+                    newRow, newCol = currentRow + rowOffset, currentCol + colOffSet
+                    
+                    if not (0 <= newRow < rows and 0 <= newCol < cols):
+                        continue
+                    # print(newRow, newCol)
+                    if grid[newRow][newCol] >= currentScore and (newRow, newCol) not in visited:
+                        visited.add((newRow, newCol))
+                        queue.append((newRow, newCol))
+            return False
+
+        low, high = 0, min(grid[0][0], grid[-1][-1])
+        while low < high:
+            mid = low + (high - low) // 2
+            print(mid)
+            if bfs(mid):
+                low = mid+1
+            else:
+                high = mid - 1
+
+        return low - 1 if not bfs(low) else low
