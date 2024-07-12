7cf1a3e8c3c76ee05757ec37f1101ef1a8250fe3
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-11 17:53:32 -0700

    Time: 330 ms (64.83%), Space: 18.1 MB (12.42%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,29 +1,36 @@
 class Solution:
-    def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
+    def findTheCity(
+        self, n: int, edges: List[List[int]], distanceThreshold: int
+    ) -> int:
         adj = defaultdict(list)
         for node1, node2, weight in edges:
             adj[node1].append((weight, node2))
             adj[node2].append((weight, node1))
-
-        smallestNo = float('inf')
+        smallestNo = float("inf")
         smallestCity = -1
 
-        for start in range(n):
-            minHeap = [(0, start)]
-            distances = {start: 0}
+        for node in range(n):
+           
+            cities = defaultdict(list)
+            cities[node] = 0
+            
+
+            minHeap = [(0, node)]
+
             while minHeap:
                 currentWeight, currentNode = heappop(minHeap)
+                print(currentWeight, currentNode)
                 if currentWeight > distanceThreshold:
                     continue
+
+                # get all the negoihbor which satisfy the threshold
                 for neighborWeight, neighborNode in adj[currentNode]:
                     newWeight = currentWeight + neighborWeight
-                    if newWeight <= distanceThreshold and (neighborNode not in distances or newWeight < distances[neighborNode]):
-                        distances[neighborNode] = newWeight
+                    if newWeight <= distanceThreshold and (neighborNode not in cities or newWeight < cities[neighborNode]):
+                        cities[neighborNode] = newWeight
                         heappush(minHeap, (newWeight, neighborNode))
-            
-            reachableCities = len(distances) - 1  # Exclude the start city itself
-            if reachableCities < smallestNo or (reachableCities == smallestNo and start > smallestCity):
-                smallestNo = reachableCities
-                smallestCity = start
-
+            reachableCity = len(cities) - 1
+            if reachableCity < smallestNo or (reachableCity == smallestNo and node > smallestCity):
+                smallestNo = reachableCity
+                smallestCity = node
         return smallestCity
