014dc1856f239e50e4166bcb1fe643035e642a54
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-13 13:11:13 -0700

    Time: 34 ms (66.67%), Space: 16.5 MB (64.32%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
@@ -1,11 +1,18 @@
 class Solution:
     def monotoneIncreasingDigits(self, n: int) -> int:
+        # convert to digits
         digits = [int(digit) for digit in str(n)]
         breakingIndex=len(digits)
+
+        # Find the breaking point when monotonic breaks
         for index in range(len(digits)-1,0,-1):
             if digits[index]<digits[index-1]:
                 digits[index-1]-=1
                 breakingIndex =index
+                
+        # replace all the digits with 9 as <= is allowed, 99 is valid and 89 is also valid
         digits[breakingIndex:]=[9]*(len(digits)-breakingIndex)
+
+        # map(str,digits) -> basically converts all the int to str and map basically calls str for each element in digits
         return int("".join(map(str, digits)))
                 
