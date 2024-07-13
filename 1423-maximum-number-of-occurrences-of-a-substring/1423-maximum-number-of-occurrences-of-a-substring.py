eea1e0fbcd1f7971b463bf6676900ce033ba2bc0
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-13 09:46:52 -0700

    Time: 112 ms (84.91%), Space: 18.1 MB (69.65%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
@@ -1,18 +1,30 @@
 class Solution:
     def maxFreq(self, s: str, maxLetters: int, minSize: int, maxSize: int) -> int:
-        maxOccurence, maxOccurringSubString = 0, ""
-        substringFrequency = defaultdict(int)
+        # maxOccurence, maxOccurringSubString = 0, ""
+        # substringFrequency = defaultdict(int)
 
-        def isValidSubstring(substring: str) -> bool:
-            return len(set(substring) )<= maxLetters
+        # def isValidSubstring(substring: str) -> bool:
+        #     return len(set(substring) )<= maxLetters
 
-        for currentSize in range(minSize, maxSize + 1):
-            for index in range(len(s) - currentSize + 1):
-                subString = s[index : index + currentSize]
-                if isValidSubstring(subString):
-                    substringFrequency[subString] += 1
-                    if substringFrequency[subString] > maxOccurence:
-                        maxOccurence = substringFrequency[subString]
-                        maxOccurringSubString = subString
+        # for currentSize in range(minSize, maxSize + 1):
+        #     for index in range(len(s) - currentSize + 1):
+        #         subString = s[index : index + currentSize]
+        #         if isValidSubstring(subString):
+        #             substringFrequency[subString] += 1
+        #             if substringFrequency[subString] > maxOccurence:
+        #                 maxOccurence = substringFrequency[subString]
+        #                 maxOccurringSubString = subString
 
-        return maxOccurence
+        # return maxOccurence
+        lookup = defaultdict(int)
+
+        for i in range(minSize, len(s)+1):
+            subarray = s[i-minSize:i]
+
+            if len(set(subarray)) <= maxLetters:
+                lookup[subarray] += 1
+        
+        if lookup:
+            return max(lookup.values())
+        else:
+            return 0
