2b17f9c0f3060fdbaf34da95d2cab3f46f509890
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-13 13:11:56 -0700

    Time: 31 ms (81.25%), Space: 16.3 MB (95.31%) - LeetHub

--- 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
+++ 0738-monotone-increasing-digits/0738-monotone-increasing-digits.py
@@ -9,10 +9,11 @@ class Solution:
             if digits[index]<digits[index-1]:
                 digits[index-1]-=1
                 breakingIndex =index
-                
+
         # replace all the digits with 9 as <= is allowed, 99 is valid and 89 is also valid
         digits[breakingIndex:]=[9]*(len(digits)-breakingIndex)
 
         # map(str,digits) -> basically converts all the int to str and map basically calls str for each element in digits
+        # Alternative is int("".join([str(x) for x in digits])
         return int("".join(map(str, digits)))
                 
