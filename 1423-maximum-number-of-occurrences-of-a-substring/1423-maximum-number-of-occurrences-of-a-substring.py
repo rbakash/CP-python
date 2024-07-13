793c0746800f2ed1fd52f17f18c14409bf73a343
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-13 09:42:19 -0700

    Time: 1914 ms (14.27%), Space: 108.9 MB (20.23%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.py
@@ -15,29 +15,4 @@ class Solution:
                         maxOccurence = substringFrequency[subString]
                         maxOccurringSubString = subString
 
-        # start, uniqueCharacters = 0, 0
-        # for end in range(len(s)):
-
-        #     # expand the window
-        #     if s[end] not in characterFrequency:
-        #         uniqueCharacters += 1
-        #     characterFrequency[s[end]] += 1
-
-        #     if (end - start)+1 >= minSize:
-        #         if (end - start)+1 > maxSize or uniqueCharacters > maxLetters:
-        #             # shrink the window
-        #             while (end - start)+1 > maxSize or uniqueCharacters > maxLetters:
-        #                 characterFrequency[s[start]] -= 1
-        #                 if characterFrequency[s[start]] == 0:
-        #                     uniqueCharacters -= 1
-        #                     del characterFrequency[s[start]]
-        #                 start += 1
-        #             if (end - start)+1 >= minSize:
-        #                 substringFrequency[s[start : end + 1]] += 1
-        #         else:
-        #             # valid string if maxLetter
-        #             substringFrequency[s[start : end + 1]] += 1
-
-        # for substring,count in substringFrequency.items():
-
         return maxOccurence
