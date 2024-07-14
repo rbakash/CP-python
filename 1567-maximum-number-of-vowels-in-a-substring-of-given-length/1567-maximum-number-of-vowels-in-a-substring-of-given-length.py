c54ec120c0ef919f1c6b047fd1fb7fc30a55218a
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-13 20:10:58 -0700

    Time: 133 ms (43.95%), Space: 17.4 MB (16.5%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.py
@@ -7,12 +7,14 @@ class Solution:
             if character in vowels:
                 currentVowels += 1
         maxVowels = currentVowels
+        
         for end in range(k, len(s)):
 
             if s[end - k] in vowels:
                 currentVowels -= 1
             if s[end] in vowels:
                 currentVowels += 1
+
             maxVowels = max(maxVowels, currentVowels)
 
         return maxVowels
