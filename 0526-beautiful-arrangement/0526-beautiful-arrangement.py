64f0389679b88f9c498204c9a2c03dee3022c3de
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-14 09:09:29 -0700

    Time: 1244 ms (5.97%), Space: 16.5 MB (37.09%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -3,21 +3,27 @@ class Solution:
         visited =set()
         numbers = list(range(n+1))
         numberOfArrangements = 0
+        
         def backTrack(currentArrangement, index,length)->int:
             nonlocal numberOfArrangements
+            # Valid Arrangements
             if len(currentArrangement) == n:
                 numberOfArrangements+=1
                 return numberOfArrangements
 
             for iterator in range(1,n+1):
+
                 if numbers[iterator] not in visited and (((len(currentArrangement)+1) % numbers[index] == 0 ) or (numbers[index] % (len(currentArrangement) +1) == 0)):
+                    # Add the current combination
                     currentArrangement.append(numbers[iterator])
                     visited.add(numbers[iterator])
+
                     backTrack(currentArrangement, iterator, len(currentArrangement))
-                    # print(visited, currentArrangement)
+
+                    # Backtrack 
                     currentArrangement.pop()
                     visited.remove(numbers[iterator])
             return numberOfArrangements
-        backTrack([],1,0)
-        return numberOfArrangements
+        
+        return backTrack([],1,0)
         
