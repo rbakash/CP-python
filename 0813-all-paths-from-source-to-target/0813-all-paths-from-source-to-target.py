6d46055b2129eaa42a5d9121977d0c83e05c2d5e
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-14 14:15:47 -0700

    Time: 78 ms (29.19%), Space: 18.3 MB (29.67%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.py
@@ -1,19 +1,19 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        allPaths=[]
-        destination = len(graph)-1
-        visited = set()
+        allPaths = []
+        destination = len(graph) - 1
+
         def dfs(path, currentNode):
-            if currentNode == destination :
+
+            if currentNode == destination:
                 allPaths.append(path)
                 return
-            # path.append(currentNode)
-            visited.add(currentNode)
+                
             for neighbor in graph[currentNode]:
                 if neighbor == destination:
-                    allPaths.append(path+[neighbor])
+                    allPaths.append(path + [neighbor])
                     continue
-                dfs(path+[neighbor],neighbor)
-                
-        dfs([0],0)
+                dfs(path + [neighbor], neighbor)
+
+        dfs([0], 0)
         return allPaths
