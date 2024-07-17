dc3eb83a072d43d4752024a24842d12ed0c718d5
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-16 18:00:54 -0700

    Time: 264 ms (86.29%), Space: 18.4 MB (97.32%) - LeetHub

--- 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.py
+++ 0808-number-of-matching-subsequences/0808-number-of-matching-subsequences.py
@@ -14,19 +14,30 @@ class Solution:
         #         matchingSubsequence += 1
 
         # this is next Level logic as stated in editorial, idk how it works
-        wordCharacterMap=defaultdict(list)
-        matchingSubsequence = 0
+        # wordCharacterMap=defaultdict(list)
+        # matchingSubsequence = 0
+        # for word in words:
+        #     wordCharacterMap[word[0]].append(word)
+
+        # for character in s:
+        #     currentWords = wordCharacterMap[character]
+        #     wordCharacterMap[character] = []
+
+        #     for eachWord in currentWords:
+        #         if len(eachWord) ==1:
+        #             matchingSubsequence += 1
+        #         else:
+        #             wordCharacterMap[eachWord[1]].append(eachWord[1:])
+
+        # return matchingSubsequence
+
+        matching_subseq_count = 0
         for word in words:
-            wordCharacterMap[word[0]].append(word)
-
-        for character in s:
-            currentWords = wordCharacterMap[character]
-            wordCharacterMap[character] = []
-
-            for eachWord in currentWords:
-                if len(eachWord) ==1:
-                    matchingSubsequence += 1
-                else:
-                    wordCharacterMap[eachWord[1]].append(eachWord[1:])
-
-        return matchingSubsequence
+            current_index = -1
+            for char in word:
+                current_index = s.find(char, current_index + 1)
+                if current_index == -1:
+                    break
+            if current_index != -1:
+                matching_subseq_count += 1
+        return matching_subseq_count
