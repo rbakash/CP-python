2a7de8b92b461f225c243a231adbffdacbd3ba92
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-17 21:58:05 -0700

    Time: 94 ms (26.01%), Space: 17.5 MB (9.29%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False
+        
+        # find the character with odd frequency
+        frequency = defaultdict(int)
+        for character in s:
+            frequency[character]+=1
+        oddFrequency =0
+        
+        for character,count in frequency.items():
+            if count % 2 == 1:
+                oddFrequency+=1
+        # We can create at most oddFrequency palindromes 
+        # as each odd frequency character need to be in seperate palindrome
+        # Ex : aabc, a = 2, b =1 , c = 1,
+        # palindromes can be aba, aca so you can create minimum of 2 palindromes
+        if oddFrequency <=  k:
+            return True
+        else:
+            return False
