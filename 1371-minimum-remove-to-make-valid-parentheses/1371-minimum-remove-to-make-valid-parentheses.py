dd4ea5acf8d329c3fc983e16940b803dd1aab7ae
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-17 20:26:58 -0700

    Time: 119 ms (15.72%), Space: 21.5 MB (5.22%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -8,11 +8,11 @@ class Solution:
 
         pointerO, pointerC = 0, 0
         indexesToDelete = set()
-        print(freqOfBrackets)
+
         while pointerO < len(freqOfBrackets["("]) and pointerC < len(freqOfBrackets[")"]):
-            print(freqOfBrackets[")"][pointerC], freqOfBrackets[")"][pointerO])
+            
             if freqOfBrackets[")"][pointerC] < freqOfBrackets["("][pointerO]:
-                print(s[freqOfBrackets[")"][pointerC]])
+
                 # invalid Close bracket, we need to remove it tp make it valid
                 indexesToDelete.add(freqOfBrackets[")"][pointerC])
                 pointerC += 1
@@ -20,7 +20,6 @@ class Solution:
                 pointerO += 1
                 pointerC += 1
 
-        print(s, pointerO, pointerC, pointerO < len(freqOfBrackets["("]))
         # remove remaining invalid brackets
         while pointerO < len(freqOfBrackets["("]):
             indexesToDelete.add(freqOfBrackets["("][pointerO])
@@ -29,6 +28,7 @@ class Solution:
         while pointerC < len(freqOfBrackets[")"]):
             indexesToDelete.add(freqOfBrackets[")"][pointerC])
             pointerC += 1
+        
         result = ""
         for index in range(len(s)):
             if index not in indexesToDelete:
