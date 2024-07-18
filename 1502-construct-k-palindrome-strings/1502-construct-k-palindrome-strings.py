4d3e3556358f2c39a66db73813e09a9f996d4e05
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-17 22:01:16 -0700

    Time: 80 ms (47.37%), Space: 17.5 MB (9.29%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -1,5 +1,6 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
+        # impossible to create k palindromes if its more than len(s)
         if k > len(s):
             return False
         
@@ -12,6 +13,7 @@ class Solution:
         for character,count in frequency.items():
             if count % 2 == 1:
                 oddFrequency+=1
+
         # We can create at most oddFrequency palindromes 
         # as each odd frequency character need to be in seperate palindrome
         # Ex : aabc, a = 2, b =1 , c = 1,
