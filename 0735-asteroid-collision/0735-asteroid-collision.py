513073709068482f2796efe9c44af476a40d5427
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-18 17:46:27 -0700

    Time: 88 ms (25.24%), Space: 17.8 MB (36.49%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,15 +3,18 @@ class Solution:
         stack = []
 
         for asteriod in asteroids:
+            flag = True
             while stack and stack[-1] > 0 and asteriod < 0:
                 if stack[-1] < abs(asteriod):
                     stack.pop()
                 elif stack[-1] == abs(asteriod):
+                    flag = False
                     stack.pop()
                     break
                 else:
+                    flag = False
                     break
-            else:
+            if flag:
                 stack.append(asteriod)
 
         return stack
