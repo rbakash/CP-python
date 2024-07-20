ddcd4feec44595a7a365c5528e389e1b9ffc292d
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-19 17:32:11 -0700

    Time: 43 ms (81.89%), Space: 17.2 MB (89.78%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -5,22 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
-        adjList = defaultdict(int)
-        count=defaultdict(int)
+    def findDuplicateSubtrees(
+        self, root: Optional[TreeNode]
+    ) -> List[Optional[TreeNode]]:
+        treeIds = defaultdict(int)
+        count = defaultdict(int)
         result = []
 
         def dfs(currentNode):
             if currentNode == None:
                 return 0
-            subTree = (currentNode.val),dfs(currentNode.left),dfs(currentNode.right)
-            if subTree not in adjList:
-                adjList[subTree]= len(adjList)+1
-            id = adjList[subTree]
-            count[id]+=1
-            if count[id] ==2:
+            triplet = (currentNode.val), dfs(currentNode.left), dfs(currentNode.right)
+            if triplet not in treeIds:
+                treeIds[triplet] = len(treeIds) + 1
+            id = treeIds[triplet]
+            count[id] += 1
+            if count[id] == 2:
                 result.append(currentNode)
-            return id 
+            return id
 
         dfs(root)
         return result
