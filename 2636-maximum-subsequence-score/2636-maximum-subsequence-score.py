b2b40f2f2033af041325d136890d74fc19053c51
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-23 14:00:53 -0700

    Time: 906 ms (9.36%), Space: 38.8 MB (25.69%) - LeetHub

--- 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
+++ 2636-maximum-subsequence-score/2636-maximum-subsequence-score.py
@@ -1,17 +1,17 @@
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        pairs=[(num1,num2) for num1,num2 in zip(nums1,nums2)]
-        pairs.sort(key=lambda x:x[1],reverse=True)
+        pairs = [(num1, num2) for num1, num2 in zip(nums1, nums2)]
+        pairs.sort(key=lambda x: x[1], reverse=True)
         print(pairs)
-        minHeap=[]
-        maxSum =0
+        minHeap = []
+        maxSum = 0
         currentN1Sum = 0
-        for eachN1,eachN2 in pairs:
+        for eachN1, eachN2 in pairs:
             currentN1Sum += eachN1
-            heappush(minHeap,eachN1)
+            heappush(minHeap, eachN1)
 
             if len(minHeap) > k:
                 currentN1Sum -= heappop(minHeap)
-            if len(minHeap) ==k:
+            if len(minHeap) == k:
                 maxSum = max(maxSum, currentN1Sum * eachN2)
         return maxSum
