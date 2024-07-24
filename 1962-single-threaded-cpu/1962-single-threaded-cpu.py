45dd96f1259d021703b6ce7a3e31b8b4fc6439b4
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-23 17:22:52 -0700

    Time: 1388 ms (89.18%), Space: 65.8 MB (20.93%) - LeetHub

--- 1962-single-threaded-cpu/1962-single-threaded-cpu.py
+++ 1962-single-threaded-cpu/1962-single-threaded-cpu.py
@@ -1,24 +1,24 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
         # sort the task based on Arrival time
-        sortedTasks = [(task[0],task[1],index) for index,task in enumerate(tasks)]
-        sortedTasks.sort(key=lambda task: task[0])
+        sortedTasks = [(avaiableAt,processingTime,index) for index,(avaiableAt,processingTime) in enumerate(tasks)]
+        sortedTasks.sort()
 
         # minHeap to maintain the smallest processing job first
         minHeap=[]
         currentExecutionTime= sortedTasks[0][0]
-        nextAvaliableTask=0
+        nextTaskIndex=0
         order=[]
         while len(order) < len(sortedTasks):
 
-            while nextAvaliableTask < len(sortedTasks) and sortedTasks[nextAvaliableTask][0] <= currentExecutionTime:
-                heappush(minHeap,(sortedTasks[nextAvaliableTask][1],sortedTasks[nextAvaliableTask][2]))
-                nextAvaliableTask+=1
+            while nextTaskIndex < len(sortedTasks) and sortedTasks[nextTaskIndex][0] <= currentExecutionTime:
+                heappush(minHeap,(sortedTasks[nextTaskIndex][1],sortedTasks[nextTaskIndex][2]))
+                nextTaskIndex+=1
 
             if minHeap:
                 nextTask=heappop(minHeap)
                 currentExecutionTime += nextTask[0]
                 order.append(nextTask[1])
             else:
-                currentExecutionTime = sortedTasks[nextAvaliableTask][0]
+                currentExecutionTime = sortedTasks[nextTaskIndex][0]
         return order
