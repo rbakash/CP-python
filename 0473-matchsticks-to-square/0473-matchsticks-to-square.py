393ef3b38014c809abda5b9d17dda923ec3b877f
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-25 20:43:08 -0700

    Time: 129 ms (92.61%), Space: 16.6 MB (38.52%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.py
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.py
@@ -1,21 +1,28 @@
 class Solution:
     def makesquare(self, matchSticks: List[int]) -> bool:
-        eachSide = sum(matchSticks)//4
-        length = len(matchSticks)
-        if sum(matchSticks)/4 != eachSide:
+        eachSide = sum(matchSticks)
+        if eachSide % 4 != 0:
             return False
-        sides=[0]*4
+        eachSide = eachSide // 4
+        length = len(matchSticks)
+        
+        sides = [0] * 4
         matchSticks.sort(reverse=True)
+
         def dfs(currentIndex):
             if currentIndex == length:
                 return True
             for sideIndex in range(4):
+                if sides[sideIndex] + matchSticks[currentIndex] > eachSide:
+                    continue
+                if sideIndex > 0 and sides[sideIndex] == sides[sideIndex-1]:
+                    continue
+                
                 if sides[sideIndex] + matchSticks[currentIndex] <= eachSide:
                     sides[sideIndex] += matchSticks[currentIndex]
-                    if dfs(currentIndex+1):
+                    if dfs(currentIndex + 1):
                         return True
                     sides[sideIndex] -= matchSticks[currentIndex]
             return False
-        return dfs(0)
-                    
 
+        return dfs(0)
