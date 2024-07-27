ab7e94be4e6e5eb6ddc7e930cb916d43dedf6537
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-27 12:17:58 -0700

    Time: 83 ms (42.48%), Space: 18.2 MB (31.47%) - LeetHub

---
+++ 2475-largest-palindromic-number/2475-largest-palindromic-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def largestPalindromic(self, num: str) -> str:
+        digitFreq = defaultdict(int)
+        num = list(num)
+        for digit in num:
+            digitFreq[(digit)]+=1
+
+        res=[]
+        for digit in '9876543210': 
+            res.append(digitFreq[digit]//2 * digit)
+        mid = ''
+        res = "".join(res).lstrip('0')
+        
+        for digit in digitFreq:
+            if digitFreq[digit]%2:
+                mid=max(digit,mid)
+        print(mid)
+        return (res+mid+res[::-1]) if (res+mid+res[::-1]) else '0'
