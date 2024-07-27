232c249d3308f1fc5df7b839eaaf8ab71f7102bd
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-27 14:25:19 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,27 +1,30 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        result = 0
+        result = ""
         sign = 1
-        index=0
-        INT_MAX = (2**31)-1
-        INT_MIN = -(2**31)
+        index = 0
 
         s = s.lstrip(" ")
         if not s:
             return 0
-        if s[0] =="+":
-            index +=1
-            sign =1
-        elif s[0] =="-":
-            sign=-1
-            index+=1
-        while index< len(s) and s[index].isdigit():
-            digit = int(s[index])
-            # Check if we can add digit to result by comparing result and int max till last digit
-            if (result > INT_MAX//10) or (result == INT_MAX//10 and digit > INT_MAX%10):
-                return INT_MAX if sign == 1 else INT_MIN
 
-            result = result*10 + digit 
-            index+=1
-    
-        return sign * result
+        if s[0] == "+":
+            index += 1
+            sign = 1
+        elif s[0] == "-":
+            sign = -1
+            index += 1
+
+        for itx in range(index, len(s)):
+            if s[itx].isdigit():
+                result += s[itx]
+            else:
+                break
+        if result == "" or result in "-+":
+            return 0
+        elif int(result) > (2**31) - 1:
+            return (2**31) - 1
+        elif int(result) < -(2**31):
+            return -(2**31)
+        else:
+            return sign * int(result)
