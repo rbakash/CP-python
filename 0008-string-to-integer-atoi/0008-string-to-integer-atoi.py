ad3badb1af48f11b175366ee4b7f9701d1d8c5f0
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-27 14:28:05 -0700

    Time: 46 ms (11.84%), Space: 16.7 MB (17.51%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -16,15 +16,18 @@ class Solution:
             index += 1
 
         for itx in range(index, len(s)):
-            if s[itx].isdigit():
+            if s[itx].isnumeric():
                 result += s[itx]
             else:
                 break
         if result == "" or result in "-+":
             return 0
-        elif int(result) > (2**31) - 1:
+        elif sign * int(result) > (2**31) - 1:
+            print("+",sign, result)
             return (2**31) - 1
-        elif int(result) < -(2**31):
-            return -(2**31)
+        elif sign * int(result) < -2**31:
+            print("-",sign, result)
+            return -2**31
         else:
+            print(sign, result)
             return sign * int(result)
