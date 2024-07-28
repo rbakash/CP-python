31e00aad3cf4b677cca4dddd4a8c21fd72f3472b
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-28 12:17:28 -0700

    Time: 37 ms (71.18%), Space: 16.6 MB (38.6%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,28 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        self.validIps=[]
+        def isValidIp(ip:str)->bool:
+            print(ip)
+            subNets = ip.split(".")
+            if len(subNets)!=4:
+                return False
+            for eachSubNet in subNets:
+                if not eachSubNet.isdigit() or int(eachSubNet) < 0 or int(eachSubNet) > 255:
+                    return False
+                if len(eachSubNet) > 1 and eachSubNet[0] == '0':
+                    return False
+            return True
+        def backTrack(prefix, suffix,dots):
+            if dots> 3 or not suffix:
+                return
+            if dots ==3:
+                if isValidIp(prefix+suffix):
+                    self.validIps.append(prefix+suffix)
+                return
+            for index in range(1,min(4,len(suffix)+1)):
+               
+                backTrack(prefix + suffix[:index]+".",suffix[index:],dots+1)
+               
+            return
+        backTrack("",s,0)
+        return self.validIps
