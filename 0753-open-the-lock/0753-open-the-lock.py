65c95c2c6e24187a2cf26be47d2175ccb3c3e9a7
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-29 16:36:55 -0700

    Time: 1025 ms (10.45%), Space: 18 MB (64.04%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.py
+++ 0753-open-the-lock/0753-open-the-lock.py
@@ -6,26 +6,26 @@ class Solution:
         visited = set()
         while queue:
             currentLevel = len(queue)
-            currentCombination,currentMove = queue.popleft()
-           
-
-            if currentCombination == target:
-                return currentMove
+            for index in range(currentLevel):
+                currentCombination,currentMove = queue.popleft()
+                
+                if currentCombination == target:
+                    return currentMove
 
                 # every wheel combination
-            for wheelIndex in range(4):
-                # Each of the wheel can have 2 combination
-                # Ex: 0000-> 1000 and 9000
-                for delta in [-1,1]:
-                    if currentCombination[wheelIndex] =='0' and delta==-1:
-                        newWheel = 9
-                    else:
-                        newWheel = (int(currentCombination[wheelIndex])+ delta)%10
-                    newCombination = currentCombination[:wheelIndex]+str(newWheel) + currentCombination[wheelIndex+1:]
+                for wheelIndex in range(4):
+                    # Each of the wheel can have 2 combination
+                    # Ex: 0000-> 1000 and 9000
+                    for delta in [-1,1]:
+                        if currentCombination[wheelIndex] =='0' and delta==-1:
+                            newWheel = 9
+                        else:
+                            newWheel = (int(currentCombination[wheelIndex])+ delta)%10
+                        newCombination = currentCombination[:wheelIndex]+str(newWheel) + currentCombination[wheelIndex+1:]
 
-                    if newCombination not in visited and newCombination not in deadends:
-                        queue.append((newCombination,currentMove+1))
-                        visited.add(newCombination)
+                        if newCombination not in visited and newCombination not in deadends:
+                            queue.append((newCombination,currentMove+1))
+                            visited.add(newCombination)
         return -1
 
                         
