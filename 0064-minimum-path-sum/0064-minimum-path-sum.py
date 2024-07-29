2442af2fea3730c033d87db8b0d1ded17363ec2f
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-28 19:19:58 -0700

    Time: 87 ms (45.27%), Space: 18.5 MB (26.89%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -2,18 +2,19 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         maxRow = len(grid)
         maxCol = len(grid[0])
-        dp=[[0] * maxCol for _ in range(maxRow)]
+        dp = [[0] * maxCol for _ in range(maxRow)]
         print(dp)
-        dp[maxRow-1][maxCol-1]=grid[maxRow-1][maxCol-1]
+        dp[maxRow - 1][maxCol - 1] = grid[maxRow - 1][maxCol - 1]
 
-        for row in range(maxRow-1,-1,-1):
-            for col in range(maxCol-1,-1,-1):
-                if col == maxCol-1 and row != maxRow-1:
-                    dp[row][col]=grid[row][col]+dp[row+1][col]
-                elif row == maxRow -1 and col != maxCol-1:
-                    dp[row][col]=grid[row][col]+ dp[row][col+1]
-                elif row != maxRow-1 and col != maxCol-1:
-                    dp[row][col]=grid[row][col]+ min(dp[row+1][col],dp[row][col+1])
-                # else:
-                #     dp[row][col]=grid[row][col]
+        for row in range(maxRow - 1, -1, -1):
+            for col in range(maxCol - 1, -1, -1):
+                # if its last col then take the next row as min cost
+                if col == maxCol - 1 and row != maxRow - 1:
+                    dp[row][col] = grid[row][col] + dp[row + 1][col]
+                # if its last row, then take the next col as min cost
+                elif row == maxRow - 1 and col != maxCol - 1:
+                    dp[row][col] = grid[row][col] + dp[row][col + 1]
+                elif row != maxRow - 1 and col != maxCol - 1:
+                    dp[row][col] = grid[row][col] + min(dp[row + 1][col], dp[row][col + 1])
+
         return dp[0][0]
