c2d77b18bcfdcfaf992bc8700f8b98d0f8a4e208
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-28 19:34:05 -0700

    Time: 93 ms (24.65%), Space: 18.3 MB (40.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -6,14 +6,20 @@ class Solution:
 
         dp[0][0] = grid[0][0]
 
-        for row in range(1,maxRow):
-            dp[row][0] = dp[row-1][0]+grid[row][0]
-        
-        for col in range(1,maxCol):
-            dp[0][col] = dp[0][col-1]+grid[0][col]
+        # for row in range(1,maxRow):
+        #     dp[row][0] = dp[row-1][0]+grid[row][0]
 
-        for row in range(1, maxRow):
-            for col in range( 1, maxCol):
-                dp[row][col] = grid[row][col] + min(dp[row - 1][col], dp[row][col - 1])
+        # for col in range(1,maxCol):
+        #     dp[0][col] = dp[0][col-1]+grid[0][col]
 
-        return dp[maxRow-1][maxCol-1]
+        for row in range( maxRow):
+            for col in range( maxCol):
+                if row == 0 and col == 0:
+                    continue
+                elif row > 0 and col > 0:
+                    dp[row][col] = grid[row][col] + min(dp[row - 1][col], dp[row][col - 1])
+                elif col == 0:
+                    dp[row][0] = dp[row - 1][0] + grid[row][0]
+                elif row == 0:
+                    dp[0][col] = dp[0][col - 1] + grid[0][col]
+        return dp[maxRow - 1][maxCol - 1]
