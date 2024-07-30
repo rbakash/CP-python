f135464559ce9eaad609a948247ea06deae7700e
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-29 22:40:57 -0700

    Time: 42 ms (96.17%), Space: 20.2 MB (67.55%) - LeetHub

--- 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
+++ 0285-inorder-successor-in-bst/0285-inorder-successor-in-bst.py
@@ -8,17 +8,26 @@
 class Solution:
 
     def inorderSuccessor(self, root: TreeNode, p: TreeNode) -> Optional[TreeNode]:
-        self.previous=None
-        self.successor = None
-        def traversal(root):
-            if root is None:
-                return 
-            traversal(root.left)
-            if self.previous == p:
-                self.successor = root
+        # self.previous=None
+        # self.successor = None
+
+        # def traversal(root):
+        #     if root is None:
+        #         return 
+        #     traversal(root.left)
+        #     if self.previous == p:
+        #         self.successor = root
                 
-            self.previous = root
-            traversal(root.right)
-            return
-        traversal(root)
-        return self.successor
+        #     self.previous = root
+        #     traversal(root.right)
+        #     return
+        # traversal(root)
+        # return self.successor
+        succesor = None
+        while root:
+            if p.val >= root.val:
+                root = root.right
+            else:
+                succesor = root
+                root=root.left
+        return succesor
