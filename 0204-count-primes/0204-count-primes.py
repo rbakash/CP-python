a21f4254dd5240534880e44976fabf2391406351
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-30 16:20:55 -0700

    Time: 609 ms (96.47%), Space: 94.4 MB (14.81%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -6,7 +6,8 @@ class Solution:
         numbers[0],numbers[1]=False,False
         for num in range(2,int(sqrt(n)+1)):
             if numbers[num]:
-                for eachMultiple in range(num*num,n,num):
-                    numbers[eachMultiple]=False
+                numbers[num * num : n : num] = [False] * len(numbers[num * num : n : num])
+                # for eachMultiple in range(num*num,n,num):
+                #     numbers[eachMultiple]=False
         return sum(numbers)
                 
