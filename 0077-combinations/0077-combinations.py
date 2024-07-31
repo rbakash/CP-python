92b4bf122898a8fa82587eb4ca4256e05f037557
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-31 10:23:23 -0700

    Time: 662 ms (92.91%), Space: 64.8 MB (21.9%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,18 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combinations = []
-        visited = set()
 
-        def backtrack(currentCombination, currentN):
+        def backtrack(currentCombination, currentNum):
 
             if len(currentCombination) == k:
                 combinations.append(currentCombination[:])
                 return
+
             need = k-len(currentCombination)
-            remain = n-currentN+1
-            available= remain - need
-            for eachNum in range(currentN, currentN+available+1 ):
+            remain = n-currentNum+1
+            availableNumbers= remain - need
+
+            for eachNum in range(currentNum, currentNum+availableNumbers+1 ):
                 currentCombination.append(eachNum)
                 backtrack(currentCombination, eachNum+1)
                 currentCombination.pop()
