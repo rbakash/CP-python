3fea943150305e6441ccd37353039406e89f30de
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-30 20:05:11 -0700

    Time: 40 ms (51.83%), Space: 16.5 MB (54.05%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.py
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.py
@@ -3,8 +3,10 @@ class Solution:
         n = len(nums)
         numSet = set(nums)
         for num in range(2**n):
-            binaryString = bin(num)[2:].zfill(n)
-            print(binaryString)
+            # binaryString = bin(num)[2:].zfill(n)
+            binaryString = bin(num)[2:]
+            if len(binaryString) != n:
+                binaryString = '0'*(n-len(binaryString)) + binaryString
             if binaryString not in numSet:
                 return binaryString
         return""
