d2238e06ebf97d176dd51dc2bf88442c4f4ef8a6
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-07-31 11:07:36 -0700

    Time: 55 ms (52.58%), Space: 16.8 MB (68.22%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,21 +1,24 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        frequency=defaultdict(int)
+        frequency = defaultdict(int)
         for num in nums:
-            frequency[num]+=1
+            frequency[num] += 1
         n = len(nums)
-        allPerms=[]
-        def backtrack(currentPerm:list)->None:
+        allPerms = []
+
+        def backtrack(currentPerm: list) -> None:
             if len(currentPerm) == n:
                 allPerms.append(currentPerm[:])
                 return
             for num in frequency:
-                if frequency[num]>0:
-                    frequency[num]-=1
-                    currentPerm.append(num)
-                    backtrack(currentPerm)
-                    frequency[num]+=1
-                    currentPerm.pop()
+                if frequency[num] ==0:
+                    continue
+                
+                frequency[num] -= 1
+                backtrack(currentPerm[:]+[num])
+                frequency[num] += 1
+
             return
+
         backtrack([])
         return allPerms
