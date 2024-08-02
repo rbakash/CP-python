41ff8c6297fe6499cafaf7db266da3f189d92d7f
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-02 13:59:08 -0700

    Time: 87 ms (32.33%), Space: 19.9 MB (13.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,31 +3,54 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+class HeapNode:
+    def __init__(self,node):
+        self.node = node
+    def __lt__(self,other):
+        return self.node.val < other.node.val
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        listLength = len(lists)
+        #---------using Min Heap
+        dummyNode = ListNode(0)
+        current=dummyNode
+        minHeap = []
+        for eachList in lists:
+            if eachList:
+                heappush(minHeap,HeapNode(eachList))
+        while minHeap:
+            print(minHeap[0].node.val)
+            currentNode = heappop(minHeap)
+            currentNode = currentNode.node
+            current.next = currentNode
+            current= current.next
+            if currentNode.next:
+                heappush(minHeap,HeapNode(currentNode.next))
+        return dummyNode.next
+            
+        # ------ Normal linked list swapping ------- 
+        # listLength = len(lists)
         
-        if listLength == 0:
-            return None
+        # if listLength == 0:
+        #     return None
         
-        if listLength == 1:
-            return None if (not lists[0]) else lists[0] 
+        # if listLength == 1:
+        #     return None if (not lists[0]) else lists[0] 
         
-        prev=lists[0]
+        # prev=lists[0]
 
-        def mergeTwoLists(list1:List[ListNode],list2:[List[ListNode]]):
+        # def mergeTwoLists(list1:List[ListNode],list2:[List[ListNode]]):
 
-            if list1 is None:
-                return list2
-            elif list2 is None:
-                return list1
-            #keep joining smaller values in comparision to other list
-            elif list1.val > list2.val:
-                list2.next = mergeTwoLists(list1, list2.next)
-                return list2
-            else:
-                list1.next = mergeTwoLists(list1.next, list2)
-                return list1
-        for index in range(1,listLength):
-            prev = mergeTwoLists(prev,lists[index])
-        return prev
+        #     if list1 is None:
+        #         return list2
+        #     elif list2 is None:
+        #         return list1
+        #     #keep joining smaller values in comparision to other list
+        #     elif list1.val > list2.val:
+        #         list2.next = mergeTwoLists(list1, list2.next)
+        #         return list2
+        #     else:
+        #         list1.next = mergeTwoLists(list1.next, list2)
+        #         return list1
+        # for index in range(1,listLength):
+        #     prev = mergeTwoLists(prev,lists[index])
+        # return prev
