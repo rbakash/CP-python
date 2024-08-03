74fca7efb7f75786ae2178493c49cb831b6a42be
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-03 10:54:16 -0700

    Time: 31 ms (86.24%), Space: 16.6 MB (39.09%) - LeetHub

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -1,16 +1,15 @@
 class Solution:
     def numSteps(self, s: str) -> int:
-        s=list(s)
-        steps=0
-
-        while len(s) >1:
-            if s[-1] =="0":
-                # Even, divide by 2
-                s.pop()
+        nearest2 = len(s)-1
+        numToReach = 2**nearest2
+        steps = 0
+        num = int(s,2)
+        print(num)
+        while num != 1:
+            if num %2:
+                # odd
+                num +=1
             else:
-                # Odd, add one
-                num = int("".join(s),2)
-                s=bin(num+1)[2:]
-                s=list(s)
+                num = num//2
             steps+=1
         return steps
