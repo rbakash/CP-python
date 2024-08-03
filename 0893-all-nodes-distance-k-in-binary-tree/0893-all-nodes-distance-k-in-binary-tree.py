cef184cdd03679f2662d491696d3c6bcfdeae733
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-03 09:58:07 -0700

    Time: 41 ms (43.97%), Space: 17 MB (26.72%) - LeetHub

--- 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
+++ 0893-all-nodes-distance-k-in-binary-tree/0893-all-nodes-distance-k-in-binary-tree.py
@@ -5,39 +5,44 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         # Build the graph to include root edge along with left,right
-        parent=defaultdict(int)
+        parent = defaultdict(int)
         stack = [root]
+
         def createGraph():
             while stack:
                 currentNode = stack.pop()
                 if currentNode.left:
-                    parent[currentNode.left]=currentNode
+                    parent[currentNode.left] = currentNode
                     stack.append(currentNode.left)
                 if currentNode.right:
-                    parent[currentNode.right]=currentNode
+                    parent[currentNode.right] = currentNode
                     stack.append(currentNode.right)
+
         createGraph()
-        nodesAtKthDistance=[]
-        visited=set()
+        nodesAtKthDistance = []
+        visited = set()
+
         # Use the graph to traverse the tree
-        def dfs(root,currentDistance):
+        def dfs(root, currentDistance):
             if not root or root in visited:
                 return
+
+            visited.add(root)
             if currentDistance == k:
                 nodesAtKthDistance.append(root.val)
                 return
-            visited.add(root)
-            for eachNode in [root.left,root.right,parent[root]]:
+
+            for eachNode in [root.left, root.right, parent[root]]:
 
                 if eachNode and eachNode.val not in visited:
-                    # visited.add(root.left.val)
-                    dfs(eachNode,currentDistance+1)
+
+                    dfs(eachNode, currentDistance + 1)
 
             return
-        dfs(target,0)
-        return nodesAtKthDistance
-            
 
+        dfs(target, 0)
+        return nodesAtKthDistance
