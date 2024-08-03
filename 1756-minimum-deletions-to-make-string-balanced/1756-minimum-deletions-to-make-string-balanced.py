3bfcb1ae4097c674bec6fb7f0916045492f37df1
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-02 21:06:02 -0700

    Time: 250 ms (70.8%), Space: 17.9 MB (42.75%) - LeetHub

--- 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
+++ 1756-minimum-deletions-to-make-string-balanced/1756-minimum-deletions-to-make-string-balanced.py
@@ -1,20 +1,31 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n = len(s)
-        aCount=[0]*n
-        bCount = [0]*n
-        runningCount =0
-        minDeletion = n
-        for index in range(n):
-            bCount[index] = runningCount
-            if s[index]=='b':
-                runningCount+=1
-        runningCount = 0
-        for index in range(n-1,-1,-1):
-            aCount[index]=runningCount
-            if s[index]=='a':
-                runningCount+=1
-        
-        for i in range(n):
-            minDeletion = min(minDeletion, aCount[i]+bCount[i])
+        #Stack based 
+        stack =[]
+        minDeletion = 0
+        for char in s:
+            if stack and stack[-1] =='b' and char =='a':
+                stack.pop()
+                minDeletion +=1
+            else:
+                stack.append(char)
         return minDeletion
+
+        # n = len(s)
+        # aCount=[0]*n
+        # bCount = [0]*n
+        # runningCount =0
+        # minDeletion = n
+        # for index in range(n):
+        #     bCount[index] = runningCount
+        #     if s[index]=='b':
+        #         runningCount+=1
+        # runningCount = 0
+        # for index in range(n-1,-1,-1):
+        #     aCount[index]=runningCount
+        #     if s[index]=='a':
+        #         runningCount+=1
+        
+        # for i in range(n):
+        #     minDeletion = min(minDeletion, aCount[i]+bCount[i])
+        # return minDeletion
