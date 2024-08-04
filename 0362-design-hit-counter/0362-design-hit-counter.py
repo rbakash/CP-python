354ff4c38513d6a898eba7a41bfae38e81b8dedb
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-04 11:01:13 -0700

    Time: 35 ms (57.31%), Space: 16.7 MB (25.63%) - LeetHub

--- 0362-design-hit-counter/0362-design-hit-counter.py
+++ 0362-design-hit-counter/0362-design-hit-counter.py
@@ -1,28 +1,27 @@
 class HitCounter:
 
     def __init__(self):
-        self.hits=[]
-        self.low=0
-        self.totalHits =0    
+        self.hits = []
+        self.low = 0
+        self.totalHits = 0
 
     def hit(self, timestamp: int) -> None:
         self.hits.append(timestamp)
-        self.totalHits+=1
-        
+        self.totalHits += 1
 
     def getHits(self, timestamp: int) -> int:
         # perfomr binary search till timestamp -300
-        target = timestamp-300
-        high =self.totalHits-1
-        
-        while self.low<=high:
-            mid = (self.low+high)//2
-              
+        target = timestamp - 300
+        high = self.totalHits -1
+
+        while self.low <= high:
+            mid = (self.low + high) // 2
+
             if self.hits[mid] <= target:
-                 self.low=mid+1
+                self.low = mid + 1
             else:
-               high = mid-1
-        
+                high = mid - 1
+
         return self.totalHits - self.low
 
 
