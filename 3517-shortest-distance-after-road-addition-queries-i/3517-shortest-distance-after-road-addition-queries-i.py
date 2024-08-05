cb8ba834d7c7aa7a866b1b1f7be9cbb05f4b3d34
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-04 17:26:24 -0700

    Time: 2553 ms (20%), Space: 17.1 MB (80%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.py
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.py
@@ -2,7 +2,7 @@ class Solution:
     def shortestDistanceAfterQueries(self, n: int, queries: List[List[int]]) -> List[int]:
         adjList=defaultdict(list)
         for src in range(n-1):
-            adjList[src].append(src+1)
+            adjList[src].append((src+1,1))
         
         def bfs():
             queue=deque([0])
@@ -22,12 +22,36 @@ class Solution:
             
             return float("inf")
         
+        def djisktra()->int:
+            visited=set()
+            distance=[float("inf")]*n
+            distance[0]=0
+            minHeap = [(0,0)]
+
+            while minHeap:
+                currentNode, currentDistance = heappop(minHeap)
+                if currentNode == n-1:
+                    return distance[n-1]
+                if currentNode in visited or currentDistance > distance[currentNode]:
+                    continue
+                
+                visited.add(currentNode)
+                for eachNeighbor,weight in adjList[currentNode]:
+                    if eachNeighbor in visited:
+                        continue
+                    newDistance = currentDistance+weight
+                    if newDistance < distance[eachNeighbor]:
+                        # Update the weight
+                        distance[eachNeighbor]=newDistance
+                        heappush(minHeap,(eachNeighbor,newDistance))
+            return distance[n-1]
+                
         # find the shortestpath for first time
         answer=[]
 
         for src,dest in queries:
             # print(adjList)
-            adjList[src].append(dest)
-            shortest = bfs()
+            adjList[src].append((dest,1))
+            shortest = djisktra()
             answer.append(shortest)        
         return answer
