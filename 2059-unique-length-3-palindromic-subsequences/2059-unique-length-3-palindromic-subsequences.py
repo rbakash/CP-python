08f2a4fd4e589e111d9aaa31ed047ae28e249332
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-12 17:40:05 -0700

    Time: 518 ms (58.8%), Space: 17.3 MB (92.08%) - LeetHub

---
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.py
@@ -0,0 +1,20 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        firstOccurence,lastOccurence=[-1]*26,[-1]*26
+        for index in range(len(s)):
+            charIndex = ord(s[index])-ord('a')
+            if firstOccurence[charIndex]==-1:
+                firstOccurence[charIndex]=index
+            lastOccurence[charIndex]=index
+        noOfPalindromes =0
+        
+        for index in range(26):
+            if firstOccurence[index]==-1:
+                continue
+            uniqueCharacters=set()
+            for k in range(firstOccurence[index]+1,lastOccurence[index]):
+                uniqueCharacters.add(s[k])
+            noOfPalindromes +=len(uniqueCharacters)
+        
+        return noOfPalindromes
+            
