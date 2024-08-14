1121b1e592f547a39f116342202e93885db4e150
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-14 10:37:43 -0700

    Time: 252 ms (65.92%), Space: 23.2 MB (62.01%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,28 @@
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        adjList=defaultdict(list)
+        distance= [float("inf")]*n
+        
+        # create  the graph
+        for src,dest,weight in roads:
+            adjList[src].append((dest,weight))
+            adjList[dest].append((src,weight))
+
+        minHeap = [(0,0)]
+        noOfPaths=[0]*n
+        noOfPaths[0]=1
+        while minHeap:
+            currentWeight,currentNode = heappop(minHeap)
+            
+            for eachNeigbhor, neigbhorWeight in adjList[currentNode]:
+                newWeight= currentWeight + neigbhorWeight
+                if newWeight < distance[eachNeigbhor] :
+                    distance[eachNeigbhor]=newWeight
+                    noOfPaths[eachNeigbhor]=noOfPaths[currentNode]
+                    heappush(minHeap, (newWeight, eachNeigbhor))
+                elif newWeight == distance[eachNeigbhor] :
+                    noOfPaths[eachNeigbhor]=(noOfPaths[currentNode] + noOfPaths[eachNeigbhor]) % 1_000_000_007
+            
+        # now just count the shortest pat
+        return noOfPaths[n-1]
+
