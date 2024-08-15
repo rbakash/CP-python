ac7bb20688b53791391272bd4531aef785814b96
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-15 13:15:20 -0700

    Time: 386 ms (97.49%), Space: 17.8 MB (84.15%) - LeetHub

--- 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
+++ 0550-shortest-path-to-get-food/0550-shortest-path-to-get-food.py
@@ -2,7 +2,6 @@ class Solution:
     def getFood(self, grid: List[List[str]]) -> int:
         directions=[(0,-1),(0,1),(-1,0),(1,0)]
         rows,cols = len(grid),len(grid[0])
-        visited = set()
         startingRow,startingCol = -1,-1
 
         # find the src
@@ -11,7 +10,9 @@ class Solution:
                 if grid[row][col] == "*":
                     startingRow,startingCol = row,col
                     break
+        
         minHeap=deque([(0,startingRow,startingCol)])
+        
         while minHeap:
             currentCost,currentRow,currentCol = minHeap.popleft()
             if grid[currentRow][currentCol] =="#":
@@ -19,8 +20,8 @@ class Solution:
 
             for rowOffset,colOffset in directions:
                 newRow,newCol = currentRow+rowOffset,currentCol+colOffset
-                if 0<=newRow<rows and 0<=newCol<cols and (newRow,newCol) not in visited:
-                    visited.add((newRow,newCol))
-                    if grid[newRow][newCol] in ["O","#"]:
-                        minHeap.append((currentCost+1,newRow,newCol))
+                if 0<=newRow<rows and 0<=newCol<cols and grid[newRow][newCol] !="X":
+                    
+                    grid[newRow][newCol]='X' if grid[newRow][newCol]!='#' else '#'
+                    minHeap.append((currentCost+1,newRow,newCol))
         return -1
