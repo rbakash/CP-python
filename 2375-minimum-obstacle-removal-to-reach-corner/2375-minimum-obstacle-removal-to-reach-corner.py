106de20eb79722517dd1723b82a64654406025cb
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-14 18:21:13 -0700

    Time: 3692 ms (12.63%), Space: 57.3 MB (5.41%) - LeetHub

--- 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.py
+++ 2375-minimum-obstacle-removal-to-reach-corner/2375-minimum-obstacle-removal-to-reach-corner.py
@@ -1,12 +1,14 @@
 class Solution:
     def minimumObstacles(self, grid: List[List[int]]) -> int:
         visited=set()
-        minHeap=[(grid[0][0], grid[0][0],0,0)] # weight,noOfblockages, row,col
+        minHeap=[(grid[0][0],0,0)] # noOfblockages, row,col
         directions = [(0,-1),(0,1),(-1,0),(1,0)] # 4 directions
         rows,cols = len(grid),len(grid[0])
+        minBlockages=[[float("inf")]*cols for _ in range(rows)]
+        minBlockages[0][0] = grid[0][0]
         
         while minHeap:
-            currentWeight,currentBlockages, currentRow,currentCol = heappop(minHeap)
+            currentBlockages, currentRow,currentCol = heappop(minHeap)
             if (currentRow,currentCol) == (rows-1,cols-1):
                 return currentBlockages
             if (currentRow,currentCol) in visited:
@@ -15,5 +17,7 @@ class Solution:
             for rowOffset,colOffset in directions:
                 newRow,newCol = currentRow+rowOffset,currentCol+colOffset
                 if 0<=newRow<rows and 0<=newCol<cols and (newRow,newCol) not in visited:
-                    heappush(minHeap, (currentWeight+grid[newRow][newCol],currentBlockages+grid[newRow][newCol],newRow,newCol))
+                    newBlockages = currentBlockages+grid[newRow][newCol]
+                    if newBlockages < minBlockages[newRow][newCol]:
+                        heappush(minHeap, (currentBlockages+grid[newRow][newCol],newRow,newCol))
         return -1
