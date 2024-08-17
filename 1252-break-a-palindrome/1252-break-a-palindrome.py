c6adeca5769328d2724d5b501a58d935d6d4041a
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-17 09:21:13 -0700

    Time: 37 ms (39.87%), Space: 16.6 MB (28.52%) - LeetHub

--- 1252-break-a-palindrome/1252-break-a-palindrome.py
+++ 1252-break-a-palindrome/1252-break-a-palindrome.py
@@ -3,16 +3,12 @@ class Solution:
         if len(palindrome) ==1:
             return ""
         string = list(palindrome)
-        right =len(string)-1
-        for index in range(len(palindrome)):
-            if string[index]!="a" and string[right]!="a" and index!=right:
+
+        for index in range(len(palindrome)//2):
+            if string[index]!="a":
                 string[index]="a"
                 return "".join(string)
-            else:
-                right-=1
-        # print(right,index)
-        # if index == right:
-        #     string[left]="b"
-        # if index >right:
+            
+        
         string[len(string)-1]="b"
         return "".join(string)
