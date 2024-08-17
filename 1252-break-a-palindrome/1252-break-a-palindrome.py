a7a5d16fbbbe2193bc21c7865f82b83aa4660932
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-17 09:20:29 -0700

    Time: 33 ms (68.2%), Space: 16.3 MB (96.62%) - LeetHub

--- 1252-break-a-palindrome/1252-break-a-palindrome.py
+++ 1252-break-a-palindrome/1252-break-a-palindrome.py
@@ -7,12 +7,12 @@ class Solution:
         for index in range(len(palindrome)):
             if string[index]!="a" and string[right]!="a" and index!=right:
                 string[index]="a"
-                break
+                return "".join(string)
             else:
                 right-=1
-        print(right,index)
-        if index == right:
-            string[left]="b"
-        if index >right:
-            string[len(string)-1]="b"
+        # print(right,index)
+        # if index == right:
+        #     string[left]="b"
+        # if index >right:
+        string[len(string)-1]="b"
         return "".join(string)
