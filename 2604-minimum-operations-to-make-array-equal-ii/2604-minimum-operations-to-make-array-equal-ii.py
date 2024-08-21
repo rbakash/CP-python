c959417cd868e62bc46d0d005d2a0bb7f15665b2
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-21 15:46:23 -0700

    Time: 535 ms (55.93%), Space: 33.7 MB (28.25%) - LeetHub

--- 2604-minimum-operations-to-make-array-equal-ii/2604-minimum-operations-to-make-array-equal-ii.py
+++ 2604-minimum-operations-to-make-array-equal-ii/2604-minimum-operations-to-make-array-equal-ii.py
@@ -2,19 +2,17 @@ class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int], k: int) -> int:
         if k ==0:
             return -1 if nums1 !=nums2 else 0
-        positiveDiff,negativeDiff = 0,0
+        
+        toAdd ,toSub = 0,0
         for num1Element, num2Element in zip(nums1,nums2):
+            if abs(num1Element- num2Element)%k !=0:
+                return -1
             if num1Element > num2Element:
-                if (num1Element - num2Element)%k ==0:
-                    positiveDiff += (num1Element - num2Element)
-                else:
-                    return -1
+                toAdd  += (num1Element - num2Element)
             else:
-                if (num2Element - num1Element)%k ==0:
-                    negativeDiff += (num2Element - num1Element)
-                else:
-                    return -1
-        if positiveDiff == negativeDiff:
-            return positiveDiff//k
+                toSub += (num2Element - num1Element)
+                
+        if toAdd  == toSub:
+            return toAdd //k
         else:
             return -1
