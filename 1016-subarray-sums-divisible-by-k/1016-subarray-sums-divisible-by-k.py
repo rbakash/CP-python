f75b1304dff93997e33d45695276e8a997fed94e
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-08-30 09:32:51 -0700

    Time: 232 ms (51.92%), Space: 21.4 MB (61.75%) - LeetHub

--- 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
+++ 1016-subarray-sums-divisible-by-k/1016-subarray-sums-divisible-by-k.py
@@ -1,14 +1,14 @@
 class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
-        prefixSum =[0]*(k)
-        prefixSum[0]=1
+        prefixForRemainder =[0]*(k)
+        prefixForRemainder[0]=1
         noOfSubArrays,prefixIndex =0,0
 
         # prefix & suffix sum
         for num in nums:
             prefixIndex = (prefixIndex+(num %k)+k)%k
-            noOfSubArrays += prefixSum[prefixIndex]
-            prefixSum[prefixIndex] = prefixSum[prefixIndex]+1
+            noOfSubArrays += prefixForRemainder[prefixIndex]
+            prefixForRemainder[prefixIndex] = prefixForRemainder[prefixIndex]+1
             
         return noOfSubArrays
 
