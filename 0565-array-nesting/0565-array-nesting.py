58b9cc6cfd567ffe10c3e6d72a48153b36f93b79
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-09-08 17:46:45 -0700

    Time: 677 ms (38.12%), Space: 35.1 MB (51.93%) - LeetHub

--- 0565-array-nesting/0565-array-nesting.py
+++ 0565-array-nesting/0565-array-nesting.py
@@ -2,10 +2,8 @@ class Solution:
     def arrayNesting(self, nums: List[int]) -> int:
         sk=set()
         maxLength = 0
-        allsets=defaultdict(int)
 
         for index in range(len(nums)):
-            # sk=set()
             count =0
             while nums[index] not in sk:
                 sk.add(nums[index])
