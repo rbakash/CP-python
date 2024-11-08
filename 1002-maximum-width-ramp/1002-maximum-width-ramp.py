69bb6fc5b866fc0a0b2e860e00073ab60ad145e0
Akash Ramasamudra Basavaraj <rb.akash11@gmail.com>
2024-11-08 14:56:13 -0800

    Time: 54 ms (78.7%), Space: 23 MB (95.14%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.py
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.py
@@ -1,5 +1,5 @@
 class Solution:
-    def maxWidthRamp(self, nums: List[int]) -> int:
+    def maxWidthRampSorting(self, nums: List[int]) -> int:
         n = len(nums)
         indices = [ i for i in range(n)]
 
@@ -13,3 +13,16 @@ class Solution:
             minIndexSoFar = min(minIndexSoFar,i)    
 
         return maxRampWidth
+    # Monotonic Stack solution
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        stack =[]
+        n = len(nums)
+        for index in range(n):
+            if not stack or nums[index] < nums[stack[-1]]:
+                stack.append(index)
+        maxWidth = 0
+        for index in range(n-1,-1,-1):
+            while stack and nums[stack[-1]]<=nums[index]:
+                maxWidth = max(maxWidth, (index - stack[-1]))
+                stack.pop()
+        return maxWidth
